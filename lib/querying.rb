65c43b93931831371a1d189fae740289646f3f05
Zachary Schulz <zjschulz@gmail.com>
2019-11-13 03:12:47 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,5 +20,5 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name FROM character_books INNER JOIN characters ON character_books."
+  "SELECT characters.name, books.title FROM character_books INNER JOIN characters ON character_books.character_id = characters.id INNER JOIN books ON character_books.book_id = books.id"
 end
