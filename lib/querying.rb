f72ec5fef8c6d0209f48133ca71b9ca6ed13d5d5
Zachary Schulz <zjschulz@gmail.com>
2019-11-13 02:49:07 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series INNER JOIN"
+  "SELECT series.title FROM series INNER JOIN authors ON series.author_id = authors.id I"
 end
 
 def select_character_names_and_number_of_books_they_are_in
