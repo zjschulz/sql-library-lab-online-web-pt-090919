5b5bade7156f0a453cc14bcb7e0e95c2c92a2d7d
Zachary Schulz <zjschulz@gmail.com>
2019-11-13 02:37:41 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres"
+  "SELECT authors.name, subgenres.name"
 end
 
 def select_series_title_with_most_human_characters
